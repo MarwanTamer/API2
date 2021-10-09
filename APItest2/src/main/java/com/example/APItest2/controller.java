36760e4722228f8c0327d01ac676fa3b1f064c90
IslamNosser <islam.nosser@gmail.com>
2021-10-10 01:47:58 +0200

    remove jpa

--- APItest2/src/main/java/com/example/APItest2/controller.java
+++ APItest2/src/main/java/com/example/APItest2/controller.java
@@ -1,20 +1,20 @@
 package com.example.APItest2;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/consumer_services")
+@RequestMapping
 public class controller {
 
 
-    @Autowired
-    private CustomerDetailedInquiryRepositry repositry;
-    @PostMapping
-    public CustomerDetailedInquiry customerDetailedInquiry
+//    @Autowired
+//    private CustomerDetailedInquiryRepositry repositry;
+    @PostMapping("/api/consumer_services")
+    public String customerDetailedInquiry
     ( @RequestBody CustomerDetailedInquiry customerDetailedInquiry) {
-        return repositry.save(customerDetailedInquiry);
+    	System.out.println("Found");
+    	return "done";
 }}
